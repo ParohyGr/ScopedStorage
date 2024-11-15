1dbc90e80ed4b39bf9499d8018eb350b5f9b9a2b
Tomas Paronai <132337323+ParohyGr@users.noreply.github.com>
2024-11-15 11:19:56 +0100

    cleanup

--- app/src/main/java/com/parohy/scopedstorage/ui/home/home.kt
+++ app/src/main/java/com/parohy/scopedstorage/ui/home/home.kt
@@ -1,14 +1,8 @@
 package com.parohy.scopedstorage.ui.home
 
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
 import com.parohy.scopedstorage.R
-import com.parohy.scopedstorage.common.navigation.CenteredColumn
 import com.parohy.scopedstorage.ui.ButtonsScreen
 import com.parohy.scopedstorage.ui.SSButton
 
@@ -23,12 +17,12 @@ fun Home(
     SSButton(text = "Chcem ulozit subor", onClick = goToSave)
     SSButton(text = "Chcem nacitat subor", onClick = goToLoad)
 
-    Spacer(modifier = Modifier.height(32.dp))
-    Text(text = "Este to nie je done")
-
-    SSButton(text = "Chcem zmenit subor", onClick = {})
-    SSButton(text = "Chcem vymazat subor", onClick = goToDelete)
-    SSButton(text = "Chcem presunut subor", onClick = goToMove)
-    SSButton(text = "Dalsie divne veci", onClick = {})
+//    Spacer(modifier = Modifier.height(32.dp))
+//    Text(text = "Este to nie je done")
+//
+//    SSButton(text = "Chcem zmenit subor", onClick = {})
+//    SSButton(text = "Chcem vymazat subor", onClick = goToDelete)
+//    SSButton(text = "Chcem presunut subor", onClick = goToMove)
+//    SSButton(text = "Dalsie divne veci", onClick = {})
   }
 }
